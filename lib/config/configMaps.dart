c1426fb6a8bbac4984bac5b2b715bd566b60a28d
Amare Abewa Chanie <amare.abewa@gmail.com>
2022-05-25 17:13:00 +0300

    Delete lib/config directory
    security holes has been pathced

--- lib/config/configMaps.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-String mapKey="AIzaSyBzpA2XOCjU-LaiCLxt0i3zqhsxg-xAd4M";
-
-String serverToken="key=AAAAPgIFloE:APA91bH2Xe-VqXFNA1ns262uCVPxu65PkVojCOKCX6E1aanKEzlcb2M7ADef5afBltyWTU89ODitgAjPb87umuNzmA0Wmhcdb85-hU8L1Vd-lhleOs7p4LJPzwMN_rwB9GXHftyvdWQX";
